53acdd8e150fee6e2be2532e287aacf640924037
ChuchyX <jesuspenax11@gmail.com>
2023-02-17 16:31:18 -0500

    cambios prettier

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -5,25 +5,27 @@ import { RegisterUserDto } from '../models/RegisterUserDto';
 import { User } from '../models/User';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   private _refresh = new Subject<void>();
 
-  get refresh()
-   {
-      return this._refresh;
-   }
+  get refresh() {
+    return this._refresh;
+  }
 
+  public register(user: RegisterUserDto): Observable<any> {
+    return this.http.post<any>(
+      'https://localhost:7190/api/Auth/register',
+      user
+    );
+  }
 
-  public register(user: RegisterUserDto): Observable<any>{
-    return this.http.post<any>('https://localhost:7190/api/Auth/register', user);
-  } 
-
-  public login(user: User): Observable<string>{
-    return this.http.post('https://localhost:7190/api/Auth/login', user, { responseType: 'text'});
-  }  
+  public login(user: User): Observable<string> {
+    return this.http.post('https://localhost:7190/api/Auth/login', user, {
+      responseType: 'text',
+    });
+  }
 }
